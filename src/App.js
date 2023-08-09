10209b89bc6889ee7be968d6865cb351362de94f
malayadaniGIT <malayadani@gmail.com>
2023-08-09 23:20:52 +0530

    resume in react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Resume from './Components/Resume/Resume';
+function App() {
+  return (
+    <div className="App">
+      <Resume/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Resume/Resume.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./resume.css"
+
+
+function Resume() {
+  return (
+    <div className='resume'>
+      <h1>Malaya Ranjan Dani</h1>
+      <p>Software Develop,Educator at Geekster</p>
+      <a id="a1" href='https://www.linkedin.com/in/malaya-ranjan-dani/'>LinkedIn</a>
+      <a id="a2" href='https://github.com/malayadaniGIT'>Github</a>
+      <hr></hr>
+      <h2>Experience</h2>
+      <h4>Software developer at Amazon</h4>
+      <p>I work with amazon in transportation team,some more descreption Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est repellendus explicabo dolor! Facilis, odit quas hic incidunt, tempora quisquam similique autem maxime aperiam veritatis praesentium excepturi beatae esse nam tenetur.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est repellendus explicabo dolor! Facilis, odit quas hic incidunt, tempora quisquam similique autem maxime </p>
+      <hr></hr>
+      <h4>Educator at Geekster</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ab hic minima natus, quidem harum quia assumenda, voluptates neque rerum cum porro blanditiis odit quaerat iste unde delectus? Rem voluptatum quibusdam veritatis dolore optio. Necessitatibus repellendus porro velit nobis beatae, saepe exercitationem. Vitae, incidunt aperiam necessitatibus amet itaque, dolor doloremque dolorem rerum praesentium sit, iure similique eum voluptatum quo recusandae ipsam labore tempore minima esse optio culpa corporis nam autem.</p>
+      <hr></hr>
+      <h2>Skills</h2>
+      <ul >
+        <li>Java</li>
+        <li>JavaScript</li>
+        <li>Testing</li>
+        <li>API Designing</li>
+        <li>System Design</li>
+      </ul>
+      <h2>Hobbies</h2>
+      <ol>
+        <li>Table Tennis</li>
+        <li>Reading Books</li>
+        <li>Biography</li>
+        <li>Teaching</li>
+      </ol>
+      <hr></hr>
+    </div>
+  )
+}
+
+export default Resume
